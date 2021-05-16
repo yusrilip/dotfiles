c178b6c43e19afd68ccf2c1dcef209504cc90d1f
yuus <45791714+yusrilip@users.noreply.github.com>
2021-05-16 15:23:37 +0800

    Add files via upload

---
+++ config/polybar/launch.sh
@@ -0,0 +1,64 @@
+# vim: set ft=sh
+#!/usr/bin/env bash
+
+function set_env_vars() {
+    export ETH_INTERFACE=$(ip link show | grep enp | sed 's/.*: \(.*\):.*/\1/')
+    export WLAN_INTERFACE=$(ip link show | grep wl | sed 's/.*: \(.*\):.*/\1/' | head -1)
+    export DEFAULT_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -1)
+    export HWMON_PATH=$(for i in /sys/class/hwmon/hwmon*/temp*_input; do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)"; done | awk "END {print}" | cut -d " " -f 3)
+
+    echo $ETH_INTERFACE
+    echo $WLAN_INTERFACE
+    echo $DEFAULT_INTERFACE
+    echo $HWMON_PATH
+}
+
+function stop() {
+    if pgrep polybar; then
+        killall -q polybar
+    fi
+}
+
+function start() {
+    set_env_vars
+    if type "xrandr"; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            if pgrep bspwm; then
+                MONITOR=$m polybar --reload -c $HOME/.config/polybar/config.ini bspwmBottom &
+                MONITOR=$m polybar --reload -c $HOME/.config/polybar/config.ini bspwmTop &
+            else
+                MONITOR=$m polybar --reload -c $HOME/.config/polybar/config.ini i3wmBottom &
+                MONITOR=$m polybar --reload -c $HOME/.config/polybar/config.ini i3main &
+            fi
+        done
+    else
+        if pgrep bspwm; then
+            polybar --reload bspwmTop &
+            polybar --reload bspwmBottom &
+        else
+            polybar --reload i3main &
+            polybar --reload i3wmBottom &
+        fi
+    fi
+}
+
+function toggle() {
+    if pgrep polybar; then
+        stop
+    else
+        start
+    fi
+}
+
+case $1 in
+start | restart)
+    stop
+    start
+    ;;
+stop) stop ;;
+toggle) toggle ;;
+*)
+    echo "Say 'start', 'stop' or 'toggle' my sweetheart!"
+    exit 2
+    ;;
+esac
